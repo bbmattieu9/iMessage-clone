fe158d8d2eb3e83f3c6025f97d776d1c3eb0b7db
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-02-24 14:59:06 +0100

    [SidebarChat] add sidebarChat component to sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -26,6 +26,9 @@ function Sidebar() {
 
             <div className="sidebar__chats">
                 <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
             </div>
         </div>
     )
