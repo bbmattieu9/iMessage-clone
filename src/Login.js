22626586a87bb69e0e4f829c0f81ccb7af4adcbd
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2021-02-25 22:07:59 +0100

    [Auth] add Login component

---
+++ src/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login.css';
+
+function Login() {
+    return <div className='login'>
+            <h2>You go need login sir!</h2>
+        </div>
+}
+
+export default Login
